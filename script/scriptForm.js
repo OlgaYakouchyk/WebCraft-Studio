27c6d517a706cb1d5a3108d9a2e3890a05bd34b3
Olga Yakouchyk <Kkraisler60@gmail.com>
2024-09-12 22:33:53 -0400

    add costs section

--- script/scriptForm.js
+++ script/scriptForm.js
@@ -13,9 +13,11 @@ document
     const clientName = document.getElementById("name").value;
     const clientEmail = document.getElementById("email").value;
     const clientMessage = document.getElementById("message").value;
+    const clientPhone = document.getElementById("phone").value;
 
     console.log(clientName);
     console.log(clientEmail);
+    console.log(clientPhone);
 
     // Отправляем данные через EmailJS
     emailjs
@@ -24,12 +26,21 @@ document
         email: clientEmail,
         message: clientMessage,
         reply_to: clientEmail,
+        phone: clientPhone,
       })
       .then(
         function (response) {
           console.log("SUCCESS!", response.status, response.text);
           alert("Massage sent!");
         },
+
+        // отправляем автоответ клиетт
+
+        emailjs.send("service_lpvg7rp", "template_q1nnbgh", {
+          to_name: clientName,
+          email: clientEmail,
+        }),
+
         function (error) {
           console.log("FAILED...", error);
           alert("ERROR!!!");
