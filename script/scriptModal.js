3c9a2b679a999b06def04f1998db6f30af169a2f
Olga Yakouchyk <Kkraisler60@gmail.com>
2024-09-17 21:08:37 -0400

    add modal window with more servises

--- script/scriptModal.js
+++ script/scriptModal.js
@@ -12,7 +12,33 @@ function closeModal() {
 document.querySelector(".close-modal").addEventListener("click", closeModal);
 
 window.addEventListener("click", function (event) {
-  if (event.target === document.querySelector(".modal-window-answer")) {
+  if (
+    event.target === document.querySelector(".modal-window-answer") ||
+    event.target === this.document.querySelector(".modal-more-services")
+  ) {
     closeModal();
+  }
+});
+
+//модальное окно more services
+
+document
+  .querySelector(".btn-more-services")
+  .addEventListener("click", openModalMore);
+
+function openModalMore() {
+  document.querySelector(".modal-more-services").style.display = "block";
+}
+
+function closeModalMore() {
+  document.querySelector(".modal-more-services").style.display = "none";
+}
+document
+  .querySelector(".close-modal-more")
+  .addEventListener("click", closeModalMore);
+
+window.addEventListener("click", function (event) {
+  if (event.target === this.document.querySelector(".modal-more-services")) {
+    closeModalMore();
   }
 });
